b93a1cc46822934d86cdb4f14313ddf277a2f76c
J. David Giese <johndgiese@gmail.com>
2014-05-02 23:57:55 -0700

    Start adding socket.io to server

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 var express = require('express');
+var http = require('http');
+var socket = require('socket.io');
+
 var app = express();
+var server = http.createServer(app);
+var io = socket.listen(server);
 
 app.use(function(req, res, next){
   console.log('%s %s', req.method, req.url);
