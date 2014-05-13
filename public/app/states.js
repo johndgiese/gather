658f998f25c305a558b765ecbe682325291ec04d
J. David Giese <johndgiese@gmail.com>
2014-05-13 14:02:41 -0400

    Move templates into app directory
    Going to partition apps into separate folders.

--- public/app/states.js
+++ public/app/states.js
@@ -5,49 +5,49 @@ app.config(['$stateProvider', function($stateProvider) {
   .state('landing', {
     url: '/',
     controller: 'LandingCtrl',
-    templateUrl: 'templates/landing.html',
+    templateUrl: 'app/templates/landing.html',
   })
 
   // create a new game
   .state('createGame', {
     url: '/new',
     controller: 'CreateGameCtrl',
-    templateUrl: 'templates/create-game.html',
+    templateUrl: 'app/templates/create-game.html',
   })
 
   // create a player
   .state('createPlayer', {
     url: '/signin',
     controller: 'CreatePlayerCtrl',
-    templateUrl: 'templates/create-player.html',
+    templateUrl: 'app/templates/create-player.html',
   })
 
   // created a game, waiting for users to join
   .state('staging', {
     url: '/staging',
     controller: 'StagingCtrl',
-    templateUrl: 'templates/staging.html',
+    templateUrl: 'app/templates/staging.html',
   })
 
   // search for an existing game
   .state('search', {
     url: '/search',
     controller: 'SearchCtrl',
-    templateUrl: 'templates/search.html',
+    templateUrl: 'app/templates/search.html',
   })
 
   // joined an existing game, but waiting for it to start
   .state('waiting', {
     url: '/waiting',
     controller: 'WaitingCtrl',
-    templateUrl: 'templates/waiting.html',
+    templateUrl: 'app/templates/waiting.html',
   })
 
   // parent controller for the actual game
   .state('game', {
     url: '/game',
     controller: 'GameCtrl',
-    templateUrl: 'templates/game.html',
+    templateUrl: 'app/templates/game.html',
   });
 
 }]);
