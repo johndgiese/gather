138960ca43b4ef704c2cdf3c5df5afd0fe7bec3b
J. David Giese <johndgiese@gmail.com>
2014-05-25 16:26:36 -0400

    Customize error method for winston logger

--- server/logger.js
+++ server/logger.js
@@ -1,7 +1,7 @@
 var winston = require('winston');
 var Logger = winston.Logger;
 
-exports.logger = new Logger({
+var logger = new Logger({
   transports: [
     new winston.transports.Console({
       colorize: true,
@@ -9,3 +9,19 @@ exports.logger = new Logger({
     })
   ]
 });
+
+// extend winston logger by making it expand errors when passed in as the
+// second argument (the first argument is the log level)
+function expandErrors(logger) {
+  var oldLogFunc = logger.log;
+  logger.log = function() {
+    var args = Array.prototype.slice.call(arguments, 0);
+    if (args.length >= 2 && args[1] instanceof Error) {
+      args[1] = args[1].stack;
+    }
+    return oldLogFunc.apply(this, args);
+  };
+  return logger;
+}
+
+module.exports = expandErrors(logger);
