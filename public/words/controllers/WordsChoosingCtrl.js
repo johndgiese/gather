936c28dbe3ddcca48031e0f03cc086140ed62a18
J. David Giese <johndgiese@gmail.com>
2014-08-01 08:08:41 -0500

    Continue work on front end

--- public/words/controllers/WordsChoosingCtrl.js
+++ public/words/controllers/WordsChoosingCtrl.js
@@ -1,7 +1,23 @@
 angular.module('words')
 .controller('WordsChoosingCtrl', [
-  '$scope', '$stateParams',
-  function($scope, $stateParams) {
-  
+  '$scope', '$stateParams', 'ScopedSocket', 'gameService',
+  function($scope, $stateParams, ScopedSocket, gameService) {
+    var socket = new ScopedSocket($scope);
+    var gameState = gameService.get();
+
+    $scope.hand = gameState.custom.hand;
+
+    $scope.play = function(cardId, handIndex) {
+      var currentRound = _.last(gameState.custom.rounds);
+      socket.emit('cardChoosen', {
+        round: currentRound.id,
+        card: cardId
+      }, function(newCard) {
+        // TODO: handle errors
+        gameState.custom.hand[handIndex] = newCard;
+        $state.go('game.words.waitingForChoices');
+      });
+    };
+
   }
 ]);
--- public/words/controllers/WordsReadPromptCtrl.js
+++ public/words/controllers/WordsReadPromptCtrl.js
@@ -1,7 +1,18 @@
 angular.module('words')
 .controller('WordsReadPromptCtrl', [
-  '$scope', '$stateParams', 'gameService',
-  function($scope, $stateParams, gameService) {
-    $scope.gs = gameService.get();
+  '$scope', '$stateParams', 'ScopedSocket', 'gameService',
+  function($scope, $stateParams, ScopedSocket, gameService) {
+    var socket = new ScopedSocket($scope);
+
+    var gameState = gameService.get();
+
+    $scope.prompt = _.last(gameState.custom.rounds).prompt;
+
+    $scope.doneReading = function() {
+      socket.emit('doneReadingPrompt', {}, function() {
+        $state.go('game.words.choosing');
+      });
+    };
+
   }
 ]);
