81b98726af6d2005d8556563781ac1db4842f1c5
J. David Giese <johndgiese@gmail.com>
2014-08-23 16:29:10 -0400

    Allow users to go back to landing page from game page

--- public/join/controllers/CreateGameCtrl.js
+++ public/join/controllers/CreateGameCtrl.js
@@ -5,15 +5,19 @@ angular.module('join')
     var socket = new ScopedSocket($scope);
 
     var player = playerService.get();
+    var gameState = gameService.get();
     if (player === null) {
       stateStack.push({name: 'createGame'});
       $state.go('createPlayer');
-    } else {
+    } else if (gameState === null) {
       // move on to the next state
       // eventually game setup code will go here
       socket.emit('createGame', {type: 'words'}, function(data) {
         $state.go('game', {party: data.party});
       });
+    } else {
+      // they must have already made a game and are going backwards
+      $state.go('landing');
     }
 
   }
