7500a8ca4f86d4535387c6e28a3ff532db8ed026
J. David Giese <johndgiese@gmail.com>
2014-08-23 20:02:25 -0400

    Show your own card in the voting list

--- public/words/controllers/WordsVotingCtrl.js
+++ public/words/controllers/WordsVotingCtrl.js
@@ -1,23 +1,35 @@
 angular.module('words')
 .controller('WordsVotingCtrl', [
-  '$scope', '$stateParams', '$state', 'socket', 'gameService',
-  function($scope, $stateParams, $state, socket, gameService) {
+  '$scope', '$stateParams', '$state', 'socket', 'gameService', 'playerService',
+  function($scope, $stateParams, $state, socket, gameService, playerService) {
     var gameState = gameService.get();
+    var playerName = playerService.get().name;
 
     // shuffle the list of choices to avoid voting order bias
-    $scope.responses = _.shuffle(_.filter(gameState.custom.choices, function(c) {
-      return c.player !== gameState.you;
-    }));
+    $scope.responses = _.shuffle(gameState.custom.choices);
+    $scope.you = gameState.you;
+
+    $scope.insult = null;
+    var insults = [
+      "You can't vote for yourself, bitch.",
+      "How pathetic.  You really want to vote for yourself?  Well, you can't.",
+      "Don't be a loser and vote for yourself.",
+      playerName + ", don't be a dick and vote for yourself."
+    ];
 
     var round = _.last(gameState.custom.rounds);
     $scope.prompt = round.prompt;
 
     $scope.vote = function(response) {
-      $state.go('^.waitingForVotes');
-      socket.emit('castVote', {
-        card: response.card.id,
-        round: round.id
-      }, function() {});
+      if (response.player === gameState.you) {
+        $scope.insult = _.sample(insults);
+      } else {
+        $state.go('^.waitingForVotes');
+        socket.emit('castVote', {
+          card: response.card.id,
+          round: round.id
+        }, function() {});
+      }
     };
 
   }
