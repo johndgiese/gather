db2e8ec0c44bec0f8d6180abda78e1cded1171c5
J. David Giese <johndgiese@gmail.com>
2014-08-26 23:52:23 -0400

    Add lock service for future use

---
+++ public/util/lockService.js
@@ -0,0 +1,29 @@
+angular.module('util')
+
+.factory('lockService', ['$q',
+  function lockService($q) {
+
+    /**
+     * Decorator that ensures a promise-returning function can only have one
+     * simultaneous call at a time.  Calls to the decorated function, while
+     * waiting for an earlier call to finish, return a rejected promise.
+     * Places a `lock` property on the returned function to indicate whether it
+     * is locked.
+     */
+    return function(func) {
+      var decorated = function() {
+        if (!decorated.lock) {
+          decorated.lock = true;
+          return func.apply(null, arguments)
+          .finally(function(val) {
+            decorated.lock = false;
+          });
+        } else {
+          return $q.reject("Function is locked");
+        }
+      };
+      return decorated;
+    };
+
+  }
+]);
