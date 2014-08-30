cf7904172aa2550a63d7581d64c5bf85290f41d6
J. David Giese <johndgiese@gmail.com>
2014-08-30 11:53:08 -0500

    Add emit promise method to socket service
    This allows us to centralize error detection over the socket API

--- public/socket/socket.js
+++ public/socket/socket.js
@@ -5,5 +5,25 @@ angular.module('socket')
   var socketUrl;
   this.setUrl = function(url) { socketUrl = url; };
   this.getUrl = function() { return socketUrl; };
-  this.$get = function() { return io.connect(socketUrl); };
+  this.$get = ['$q', function($q) { 
+    var socket = io.connect(socketUrl); 
+
+    /**
+     * Return a promise for the acknowledged data.
+     * Reject if it has an `_error` field.
+     */
+    socket.emitp = function(event, data) {
+      var deferred = $q.defer();
+      this.emit(event, data, function(response) {
+        if (response._error === undefined) {
+          deferred.resolve(response);
+        } else {
+          deferred.reject(new Error(response._error));
+        }
+      });
+      return deferred.promise;
+    };
+
+    return socket;
+  }];
 });
