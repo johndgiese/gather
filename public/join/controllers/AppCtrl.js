de19357fe79f1258d8781b8852d73a4995dfa5e9
J. David Giese <johndgiese@gmail.com>
2014-09-04 11:45:48 -0500

    Fix invalid code handling

--- public/join/controllers/AppCtrl.js
+++ public/join/controllers/AppCtrl.js
@@ -16,12 +16,19 @@ angular.module('join')
     };
 
     $rootScope.$on('$stateChangeError', function(event, to, toParams, from, fromParams, error) {
+
+      if (from.name === 'app.joinGame' && to.name === 'app.game') {
+        return $state.go('app.joinGame', {invalid: toParams.party});
+      }
+
+      console.log("unhandled state change error: ");
       console.log(event);
       console.log(to);
       console.log(toParams);
       console.log(from);
       console.log(fromParams);
       console.log(error);
+
     });
 
 
