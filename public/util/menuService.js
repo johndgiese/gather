98993dc351ba6cdb13d6bae219d1cb945a4c5701
J. David Giese <johndgiese@gmail.com>
2014-09-17 14:57:04 -0500

    Revamp menu service
    No longer need to use config stuff, instead just do it in a run block.
    Also add a "remove" function, so you can register/destroy items inside
    controllers.  Finally, add the ability to register something to
    generates multiple menu items.

--- public/util/menuService.js
+++ public/util/menuService.js
@@ -1,51 +1,115 @@
 angular.module('util.menuService', [])
-.provider('menuService',
-  function MenuServiceProvider() {
+.factory('menuService', [
+  '$injector',
+  function MenuServiceFactory($injector) {
 
     items = [];
+    itemGenerators = [];
 
-    this.registerItem = registerItem;
+    function removeItem(items, id) {
+      for (var i = 0; i < items.length; i++) {
+        var item = items[i];
+        if (item.id === id) {
+          items.splice(i, 1);
+          return;
+        }
+      }
+      throw new Error("Unable to remove item");
+    }
 
-    this.$get = ['$injector', function($injector) {
-      var exports = {};
+    var exports = {};
+    idCounter = 0;
 
-      exports.currentItems = function() {
+    orderCounter = 0;
 
-        var visibleItems = _.filter(items, function(item) { 
-          return item.visible === undefined || $injector.invoke(item.visible);
+    exports.currentItems = function() {
+
+      var visibleItems = _.filter(items, function(item) { 
+        return item.visible === undefined || $injector.invoke(item.visible);
+      });
+
+      itemGenerators.forEach(function(itemGenerator) {
+        var baseOrder = itemGenerator.order;
+        var items = $injector.invoke(itemGenerator.generator);
+        items.forEach(function(item, index) {
+          item.order = baseOrder + index*0.001;
+          visibleItems.push(item);
         });
+      });
 
-        return _.map(visibleItems, function(item) {
-          var publicItem = {};
+      var sortedItems = _.sortBy(visibleItems, function(item) { return item.order; });
 
-          publicItem.action = function() {
-            return $injector.invoke(item.action);
+      return _.map(sortedItems, function(item) {
+        var publicItem = {};
+
+        publicItem.action = function() {
+          return $injector.invoke(item.action);
+        };
+
+        if (angular.isFunction(item.title)) {
+          publicItem.title = $injector.invoke(item.title);
+        } else {
+          publicItem.title = item.title;
+        }
+
+        if (item.visible !== undefined) {
+          publicItem.visible = function() {
+            return $injector.invoke(item.visible);
           };
+        }
 
-          if (angular.isFunction(item.title)) {
-            publicItem.title = $injector.invoke(item.title);
-          } else {
-            publicItem.title = item.title;
-          }
+        return publicItem;
+      });
+    };
 
-          if (item.visible !== undefined) {
-            publicItem.visible = function() {
-              return $injector.invoke(item.visible);
-            };
-          }
+    exports.registerItem = registerItem;
+    exports.registerItemGenerator = registerItemGenerator;
 
-          return publicItem;
-        });
-      };
+    return exports;
 
-      exports.registerItem = registerItem;
-
-      return exports;
-    }];
-
+    /**
+     * Register a menu item.
+     * @arg {{
+     *    title: (string|injectable),
+     *    visible?: injectable():boolean, 
+     *    action: injectable(),
+     *    order?: number
+     * }} - a menu item
+     * @returns {function} - call it to remove the item from the menu
+     */
     function registerItem(item) {
+      if (item.order === undefined) {
+        item.order = orderCounter++;
+      } else {
+        orderCounter = Math.max(orderCounter, item.order + 1);
+      }
+      item.id = idCounter++;
       items.push(item);
+      return function() {
+        removeItem(items, item.id);
+      };
+    }
+
+    /**
+     * Register an itemGenerator.  The generator property should generate an
+     * array of items, except without the `visible` property.
+     * @arg {{generator: injectable, order: number}}
+     * @returns {function} - call it to remove the item from the menu
+     */
+    function registerItemGenerator(itemGenerator) {
+      if (itemGenerator.order === undefined) {
+        itemGenerator.order = orderCounter++;
+      } else {
+        orderCounter = Math.max(orderCounter, itemGenerator.order + 1);
+      }
+
+      itemGenerator.id = idCounter++;
+
+      itemGenerators.push(itemGenerator);
+      return function() {
+        removeItem(itemGenerators, itemGenerator.id);
+      };
     }
 
   }
-);
+]);
