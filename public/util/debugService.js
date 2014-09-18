2757e999d829c93f778a23676f24a8965b7cc181
J. David Giese <johndgiese@gmail.com>
2014-09-18 13:41:27 -0400

    Turn debug service off when on live site

--- public/util/debugService.js
+++ public/util/debugService.js
@@ -1,28 +1,33 @@
 angular.module('util.debugService', [])
-.service('debugService', ['$timeout', function($timeout) {
+.service('debugService', [
+  '$timeout', '$location',
+  function($timeout, $location) {
+    if ($location.host() === 'localhost') {
 
-  var logContainer = angular.element(
-    '<div style="position: fixed; background-color: rgba(255, 255, 255, 0.8); width: 100%;"></div>'
-  );
+      var logContainer = angular.element(
+        '<div style="position: fixed; background-color: rgba(255, 255, 255, 0.8); width: 100%;"></div>'
+      );
 
-  window.onload = function() {
-    angular.element(document.body).append(logContainer);
-  };
+      window.onload = function() {
+        angular.element(document.body).append(logContainer);
+      };
 
-  function debug(message, css) {
-    if (css === undefined) {
-      css = {};
+      return function debug(message, css) {
+        if (css === undefined) {
+          css = {};
+        }
+        css = angular.extend(css, {
+          textAlign: 'center',
+          fontWeight: 'bold',
+        });
+        var entry = angular.element('<p>' + message + '</p>').css(css);
+        logContainer.append(entry);
+        $timeout(function() {
+          entry.remove();
+        }, 5000);
+      };
+    } else {
+      return function() {};
     }
-    css = angular.extend(css, {
-      textAlign: 'center',
-      fontWeight: 'bold',
-    });
-    var entry = angular.element('<p>' + message + '</p>').css(css);
-    logContainer.append(entry);
-    $timeout(function() {
-      entry.remove();
-    }, 5000);
   }
-
-  return debug;
-}]);
+]);
