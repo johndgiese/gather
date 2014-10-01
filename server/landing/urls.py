7d5dca9c76d962829e2b7fbf4c726c904162417a
J. David Giese <johndgiese@gmail.com>
2014-10-01 14:46:22 -0500

    Update home page

--- server/landing/urls.py
+++ server/landing/urls.py
@@ -5,4 +5,5 @@ from django.views.generic import TemplateView
 urlpatterns = patterns('',
     (r'^$', TemplateView.as_view(template_name="landing/normal.html")),
     (r'^ut$', TemplateView.as_view(template_name="landing/ut.html")),
+    (r'^terms$', TemplateView.as_view(template_name="terms.html")),
 )
