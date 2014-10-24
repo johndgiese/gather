988471477b4fee5cc2121b82ef91fd70bac86635
J. David Giese <johndgiese@gmail.com>
2014-10-23 22:04:34 -0400

    Add confirm mechanism to choosing controller

--- static/words/controllers/WordsChoosingCtrl.js
+++ static/words/controllers/WordsChoosingCtrl.js
@@ -1,7 +1,7 @@
 angular.module('words')
 .controller('WordsChoosingCtrl', [
-  '$scope', '$stateParams', '$state', 'socket', 'gameState', 'wordsShareService', 'lockService',
-  function($scope, $stateParams, $state, socket, gameState, wordsShareService, lockService) {
+  '$scope', '$stateParams', '$state', 'socket', 'gameState', 'wordsShareService', 'lockService', '$timeout', '$q',
+  function($scope, $stateParams, $state, socket, gameState, wordsShareService, lockService, $timeout, $q) {
 
     // copy the hand, so that you don't see the new card (just before the state
     // change) when the game state is updated
@@ -10,30 +10,47 @@ angular.module('words')
     var round = _.last(gameState.custom.rounds);
     $scope.prompt = round.prompt;
 
+    $scope.currentSelected = null;
+    var confirmTimeout = null;
     $scope.playedIndex = null;
     $scope.play = lockService.lockByGroup('ui', function(cardId, cardIndex) {
-      $scope.playedIndex = cardIndex;
-      var currentRound = _.last(gameState.custom.rounds);
-      return socket.emitp('chooseCard', {
-        round: currentRound.id,
-        card: cardId
-      })
-      .then(function(newCard) {
-        $scope.playedIndex = null;
-        $scope.hand.splice(cardIndex, 1);  // trigger leave animation
-        gameState.custom.hand[cardIndex] = newCard;
-        return $state.transition;
-      })
-      .then(function() {
-        var currentState = _.last($state.current.name.split("."));
-        if (currentState === "choosing") {
-          $state.go('^.waitingForChoices');
+      if ($scope.currentSelected !== cardIndex) {
+        // on first tap, just display confirm message
+        if (confirmTimeout !== null) {
+          $timeout.cancel(confirmTimeout);
         }
-        return $state.transition;
-      })
-      .catch(function() {
-        $scope.playedIndex = null;
-      });
+        $scope.currentSelected = cardIndex;
+        confirmTimeout = $timeout(function() {
+          $scope.currentSelected = null;
+          confirmTimeout = null;
+        }, 2500);
+        return $timeout(function() {}, 200);
+      } else {
+        // already tapped card once
+        $scope.currentSelected = null;
+        $scope.playedIndex = cardIndex;
+        var currentRound = _.last(gameState.custom.rounds);
+        return socket.emitp('chooseCard', {
+          round: currentRound.id,
+          card: cardId
+        })
+        .then(function(newCard) {
+          $scope.playedIndex = null;
+          $scope.hand.splice(cardIndex, 1);  // trigger leave animation
+          gameState.custom.hand[cardIndex] = newCard;
+          return $state.transition;
+        })
+        .then(function() {
+          var currentState = _.last($state.current.name.split("."));
+          if (currentState === "choosing") {
+            $state.go('^.waitingForChoices');
+          }
+          return $state.transition;
+        })
+        .catch(function() {
+          $scope.playedIndex = null;
+        });
+      }
     });
 
     var responseIds = _.pluck(gameState.custom.hand, 'responseId');
