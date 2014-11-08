de4e1f22c09f4ec0969afef303955ee82653ad5c
J. David Giese <johndgiese@gmail.com>
2014-11-08 14:13:42 -0600

    Get password get/set working

--- realtime_server/join/auth.js
+++ realtime_server/join/auth.js
@@ -1,19 +1,85 @@
+var Q = require('q');
+var http = require('http');
+var _ = require('underscore');
+var querystring = require('querystring');
+var config = require('../config');
+
+
+/**
+ * Post request to internal django api.
+ * @arg {String} - url path
+ * @arg {Object} - data that will be url encoded
+ */
+function booleanInternalAPIRequestProm(path, data) {
+  var deferred = Q.defer();
+
+  var dataWithSecret = _.extend({}, {secret: config['SECRET']}, data);
+  var encodedData = querystring.stringify(dataWithSecret);
+
+  var options = {
+    host: '127.0.0.1',
+    port: 8000,
+    path: path,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Length': encodedData.length,
+    }
+  };
+
+  var request = http.request(options, function(response) {
+    if (response.statusCode === 200) {
+      deferred.resolve();
+    } else {
+      deferred.reject(response.statusCode);
+    }
+  });
+
+  request.on('error', function(reason) {
+    deferred.reject(reason);
+  });
+
+  request.write(encodedData);
+  request.end();
+
+  return deferred.promise;
+};
+
+
 /**
  * Send request to the django server to create a password for the specified
- * user.  Use djanog for this, so that we can share the django and game login
+ * user.  Use djano for this, so that we can share the django and game login
  * systems.
  * @arg {String} - player email
  * @arg {String} - plain text password
- * @throws - exception if the playerId email exist in the database already
+ * @returns {Promise} - is rejected if not succesful
  */
 exports.setPassword = function(email, password) {
+  return booleanInternalAPIRequestProm('/api/set_password', {
+    email: email,
+    password: password
+  });
 };
 
 /**
  * Check the password using a call to django's server.
  * @arg {String} - player email
  * @arg {String} - plain text password
- * @returns {Boolean} - true if it matches, false if it doesn't
+ * @returns {Promise(Boolean)} - true if it matches, false if it doesn't;
+ * promise is rejected if error on transmission.
  */
 exports.checkPassword = function(email, password) {
+  return booleanInternalAPIRequestProm('/api/check_password', {
+    email: email,
+    password: password
+  })
+  .then(function(v) {
+    return true;
+  }, function(reason) {
+    if (reason === 404) {
+      return false
+    } else {
+      return Q.reject(reason);
+    }
+  });
 };
--- server/api/views.py
+++ server/api/views.py
@@ -3,26 +3,27 @@ from django.contrib.auth.hashers import check_password, make_password, is_passwo
 from django.http import HttpResponse, HttpResponseNotFound
 from django.contrib.auth import authenticate
 from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
 
 from join.models import Player
 
 
-# TODO: consider whether this is secure or not
 def internal_only(view):
     def decorated_view(request, *args, **kwargs):
-        ip_addr = request.META.get('REMOTE_ADDR')
-        if ip_addr != '127.0.0.1':
+        secret = request.POST.get('secret', None)
+        if secret is None or secret != settings.CONFIG['SECRET']:
             return HttpResponseNotFound()
         else:
             return view(request, *args, **kwargs)
     return decorated_view
 
 
-@require_GET
+@require_POST
+@csrf_exempt
 @internal_only
 def check_password_view(request):
-    email = request.GET.get('email', None)
-    password = request.GET.get('password', None)
+    email = request.POST.get('email', None)
+    password = request.POST.get('password', None)
     if (not email is None) and (not password is None):
         player = authenticate(username=email, password=password)
         if not player is None:
